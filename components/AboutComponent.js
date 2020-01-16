70608b5ad355e063bda951a01b2ae7cdac4be98e
Kasey Chong <kchong1989@gmail.com>
2020-01-15 19:21:11 -0800

    Animatable

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -4,6 +4,7 @@ import { Card, ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { baseUrl } from '../shared/baseUrl';
 import Loading from './LoadingComponent';
+import * as Animatable from 'react-native-animatable';
 
 //connecting redux stored state to props. Only connecting stored state we need--parnters
 
@@ -47,7 +48,7 @@ class About extends Component {
                     <Mission />
                     <Card
                         title='Community Partners'>
-                            <Loading />
+                        <Loading />
                     </Card>
                 </ScrollView>
             );
@@ -55,25 +56,29 @@ class About extends Component {
         if (this.props.partners.errMess) {
             return (
                 <ScrollView>
-                <Mission />
-                <Card
-                    title='Community Partners'>
-                        <Text>{this.props.partners.errMess}</Text>
-                </Card>
+                    <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                        <Mission />
+                        <Card
+                            title='Community Partners'>
+                                <Text>{this.props.partners.errMess}</Text>
+                        </Card>
+                </Animatable.View>
             </ScrollView>
             );
         }
         return (
             <ScrollView>
-                <Mission />
-                <Card
-                    title='Community Partners'>
+                <Animatable.View animation='fadeInDown' duration={2000} delay={1000}>
+                    <Mission />
+                    <Card
+                        title='Community Partners'>
                         <FlatList
                             data={this.props.partners.partners}  //first partner refers to entire state and the second partners refers to the partners data array
                             renderItem={renderPartner}
                             keyExtractor={item => item.id.toString()}
                         />
-                </Card>
+                    </Card>
+                </Animatable.View>
             </ScrollView>
         );
     }
